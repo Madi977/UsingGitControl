9893f7dac3cd42451846e9ad4d219ac5421ba21d
Madi <madi.abdimalik18@icloud.com>
2024-07-13 23:56:42 +0500

    [clean] add to new

--- UsingGitControl/ContentView.swift
+++ UsingGitControl/ContentView.swift
@@ -8,6 +8,15 @@
 import SwiftUI
 import SwiftData
 
+class Vertification{
+    
+    var data = "information"
+    
+    init(data: String){
+        self.data = "information"
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         Image(systemName: "heart.fill")
