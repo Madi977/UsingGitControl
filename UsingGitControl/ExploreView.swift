f5b1526e10a7c881a25eac47a18376db56c69a4f
Madi <madi.abdimalik18@icloud.com>
2024-07-14 01:18:03 +0500

    Crashed space

---
+++ UsingGitControl/ExploreView.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreView.swift
+//  UsingGitControl
+//
+//  Created by Madi Abdimalik on 14.07.2024.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExploreView()
+}
