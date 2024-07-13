de1e2fd69885b52cf21ef63b82f7a37cdfe61435
Madi <madi.abdimalik18@icloud.com>
2024-07-14 00:55:02 +0500

    Add image

---
+++ UsingGitControl/ImageForGit.swift
@@ -0,0 +1,19 @@
+//
+//  ImageForGit.swift
+//  UsingGitControl
+//
+//  Created by Madi Abdimalik on 14.07.2024.
+//
+
+import SwiftUI
+
+struct ImageForGit: View {
+    var body: some View {
+        Image(systemName: "person.circle.fill")
+            .font(.largeTitle)
+    }
+}
+
+#Preview {
+    ImageForGit()
+}
