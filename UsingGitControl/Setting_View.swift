3e5964e6e195c68a1e56855356586254eaf484e8
Madi <madi.abdimalik18@icloud.com>
2024-07-14 19:19:15 +0500

    [Changes] add setting view

---
+++ UsingGitControl/Setting_View.swift
@@ -0,0 +1,18 @@
+//
+//  Setting_View.swift
+//  UsingGitControl
+//
+//  Created by Madi Abdimalik on 14.07.2024.
+//
+
+import SwiftUI
+
+struct Setting_View: View {
+    var body: some View {
+        Text("Setting")
+    }
+}
+
+#Preview {
+    Setting_View()
+}
